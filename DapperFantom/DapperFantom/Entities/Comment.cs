bfaf384460055fe30a3a8f7ee3ccb33c2906c76d
David Riggins <driggins@esri.com>
2023-02-17 12:45:23 -0800

    Lesson 71 End. Create Comment Entity.

---
+++ DapperFantom/DapperFantom/Entities/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace DapperFantom.Entities
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public int ArticleId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string CommentText { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int Status { get; set; }
+    }
+}
