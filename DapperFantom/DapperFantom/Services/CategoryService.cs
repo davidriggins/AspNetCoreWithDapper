c42b6f9dd89b13e8fbe447420a5392df12548c90
David Riggins <driggins@esri.com>
2023-02-21 09:38:58 -0800

    Lesson 76 End. Category Service.

---
+++ DapperFantom/DapperFantom/Services/CategoryService.cs
@@ -0,0 +1,57 @@
+﻿using Dapper;
+using DapperFantom.Entities;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DapperFantom.Services
+{
+    public class CategoryService
+    {
+        private SqlConnection adoNetConnection;
+        private IDbConnection dapperConnection;
+        private ConnectionService connectionService;
+
+        public CategoryService(IConfiguration configuration)
+        {
+            connectionService= new ConnectionService(configuration);
+            adoNetConnection = connectionService.DbConnection();
+            dapperConnection = connectionService.ForDapper();
+        }
+
+        public List<Category> GetAllAdoNet()
+        {
+            List<Category> categories = new List<Category>();
+            SqlCommand command = new SqlCommand("select * from Category", adoNetConnection);
+            command.CommandType = CommandType.Text;
+            IDataReader dataReader = command.ExecuteReader(CommandBehavior.CloseConnection);
+            while (dataReader.Read())
+            {
+                Category category = new Category();
+                category.CategoryId = dataReader["CategoryId"] is DBNull ? 0 : int.Parse(dataReader["CategoryId"].ToString());
+                category.CategoryName = dataReader["CategoryName"] is DBNull ? string.Empty : dataReader["CategoryName"].ToString();
+                category.Slug = dataReader["Slug"] is DBNull ? string.Empty : dataReader["Slug"].ToString();
+                category.OrderBy = dataReader["OrderBy"] is DBNull ? 0 : int.Parse(dataReader["OrderBy"].ToString());
+                categories.Add(category);
+
+            }
+
+            return categories;
+        }
+
+        public List<Category> GetAllDapper()
+        {
+            List<Category> categories = new List<Category>();
+            try
+            {
+                categories = dapperConnection.Query<Category>(@"select * from Category").ToList();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return categories;
+        }
+
+    }
+}
