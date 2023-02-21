08c6d37465e37738d5a62e353d3563caf5f4dd78
David Riggins <driggins@esri.com>
2023-02-21 10:52:02 -0800

    Lesson 79 End. Create Add Article Template Part 1.

---
+++ DapperFantom/DapperFantom/Controllers/ArticleController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DapperFantom.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
