2436f6b06746c5aa9b059ddb23971cb681925637
David Riggins <driggins@esri.com>
2023-02-21 09:19:00 -0800

    Lesson 75 End. Connection Service.

---
+++ DapperFantom/DapperFantom/Services/ConnectionService.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace DapperFantom.Services
+{
+    public class ConnectionService
+    {
+        private SqlConnection myconnection;
+        private IConfiguration _configuration;
+
+        public ConnectionService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            string connectionString = _configuration.GetConnectionString("DefaultConnection").ToString();
+            myconnection = new SqlConnection(connectionString);
+            if (myconnection.State != ConnectionState.Open)
+            {
+                myconnection.Open();
+            }
+        }
+
+        internal SqlConnection DbConnection()
+        {
+            return myconnection;
+        }
+
+        internal SqlConnection ForDapper()
+        {
+            var connection = new SqlConnection(_configuration.GetConnectionString("DefaultConnection").ToString());
+            var state = connection.State;
+            if (state != ConnectionState.Open)
+            {
+                connection.Open();
+            }
+
+            return connection;
+        }
+    }
+}
