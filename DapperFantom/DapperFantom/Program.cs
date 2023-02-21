ee338bbb9b768e1e010204cf335526c8ae97cae2
David Riggins <driggins@esri.com>
2023-02-21 10:33:15 -0800

    Lesson 78 End. Display Categories in template.

--- DapperFantom/DapperFantom/Program.cs
+++ DapperFantom/DapperFantom/Program.cs
@@ -1,7 +1,11 @@
+using DapperFantom.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<CategoryService>();
 
 var app = builder.Build();
 
