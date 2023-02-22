b0bacbb4fb296f418521914617ecc132f366f2ef
David Riggins <driggins@esri.com>
2023-02-22 10:32:35 -0800

    Lesson 102 End. Navbar for Admin Panel.

--- DapperFantom/DapperFantom/Areas/Admin/Controllers/HomeController.cs
+++ DapperFantom/DapperFantom/Areas/Admin/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DapperFantom.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DapperFantom.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [AdminAuth]
     public class HomeController : Controller
     {
         public IActionResult Index()
