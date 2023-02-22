bbd0b5df609fd4222d30d4f1e2adb46bad75a32b
David Riggins <driggins@esri.com>
2023-02-22 11:24:51 -0800

    Lesson 104 End. Create User model and controller.

---
+++ DapperFantom/DapperFantom/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using DapperFantom.Models;
+using DapperFantom.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperFantom.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        private AdminService adminService;
+
+        public UserController(IServiceProvider serviceProvider)
+        {
+            adminService = serviceProvider.GetRequiredService<AdminService>();
+        }
+
+        public IActionResult Index()
+        {
+            List<Entities.Admin> userList = adminService.GetAll();
+
+            var model = new UserViewModel
+            {
+                UserList = userList,
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ DapperFantom/DapperFantom/Models/UserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using DapperFantom.Entities;
+
+namespace DapperFantom.Models
+{
+    public class UserViewModel
+    {
+        public UserViewModel()
+        {
+            Category = new();
+            Article = new();
+            CategoryList = new List<Category>();
+            ArticleList = new List<Article>();
+            CityList = new List<City>();
+            User = new Admin();
+            UserList = new List<Admin>();
+        }
+
+        public Category Category { get; set; }
+        public Article Article { get; set; }
+        public List<Category> CategoryList { get; set; }
+        public List<Article> ArticleList { get; set; }
+        public List<City> CityList { get; set; }
+        public Admin User { get; set; }
+        public List<Admin> UserList { get; set; }
+    }
+}
