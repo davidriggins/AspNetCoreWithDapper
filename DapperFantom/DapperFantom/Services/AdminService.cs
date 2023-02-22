fda94cec1c2423f7e8502db65207727fa4b70850
David Riggins <driggins@esri.com>
2023-02-21 18:01:32 -0800

    Lesson 96 End. Create Admin Service.

---
+++ DapperFantom/DapperFantom/Services/AdminService.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using DapperFantom.Entities;
+using System.Data;
+
+namespace DapperFantom.Services
+{
+    public class AdminService
+    {
+        private IDbConnection connection;
+        private ConnectionService connectionService;
+
+        public AdminService(IConfiguration configuration)
+        {
+            connectionService = new ConnectionService(configuration);
+            connection = connectionService.ForDapper();
+        }
+
+
+        public Admin Login(Admin admin)
+        {
+            Admin myAdmin = new();
+
+            try
+            {
+                var parameters = new DynamicParameters();
+                parameters.Add("@username", admin.Username);
+                parameters.Add("@password", admin.Password);
+
+                myAdmin = connection.Query<Admin>($@"select id as AdminId,Username,Password from Admins where Username=@username and Password=@password", parameters).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return myAdmin;
+        }
+    }
+}
