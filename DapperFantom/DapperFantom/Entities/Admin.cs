5a2d8a316a8376470e9570efc75e3883bac66a7c
David Riggins <driggins@esri.com>
2023-02-21 18:38:21 -0800

    Lesson 99 End. Use Data Annotations for Login Form.

--- DapperFantom/DapperFantom/Entities/Admin.cs
+++ DapperFantom/DapperFantom/Entities/Admin.cs
@@ -1,9 +1,22 @@
-﻿namespace DapperFantom.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DapperFantom.Entities
 {
     public class Admin
     {
+        [Dapper.Contrib.Extensions.Key]
         public int AdminId { get; set; }
+
+        [Required(ErrorMessage = "Please enter your user name")]
+        [MinLength(4, ErrorMessage = "Minumum length of 4 characters")]
+        [MaxLength(10, ErrorMessage = "Maximum length of 10 characters")]
+        [Display(Name = "Your Username")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter your password")]
+        [MinLength(4, ErrorMessage = "Minumum length of 4 characters")]
+        [MaxLength(10, ErrorMessage = "Maximum length of 10 characters")]
+        [Display(Name = "Your Password")]
         public string Password { get; set; }
     }
 }
