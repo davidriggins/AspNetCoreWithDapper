73e049d54a86e5f8c376111a28671a85bc02e3a6
David Riggins <driggins@esri.com>
2023-02-21 16:44:11 -0800

    Lesson 90 End. Create Article Service.

---
+++ DapperFantom/DapperFantom/Services/ArticleService.cs
@@ -0,0 +1,31 @@
+﻿using Dapper.Contrib.Extensions;
+using DapperFantom.Entities;
+using System.Data;
+
+namespace DapperFantom.Services
+{
+    public class ArticleService
+    {
+        private IDbConnection connection;
+        private ConnectionService connectionService;
+
+        public ArticleService(IConfiguration configuration)
+        {
+            connectionService = new ConnectionService(configuration);
+            connection = connectionService.ForDapper();
+        }
+
+        public int Add(Article article)
+        {
+            var result = connection.Insert(article);
+            if (result > 0)
+            {
+                return int.Parse(result.ToString());
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
