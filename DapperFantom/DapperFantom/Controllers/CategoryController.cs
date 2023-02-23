acc7ced6fe438a5939bba6329ed2784d278ba44f
David Riggins <driggins@esri.com>
2023-02-23 13:30:28 -0800

    Lesson 139 End. Create Category Controller.

---
+++ DapperFantom/DapperFantom/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using DapperFantom.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperFantom.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private CategoryService categoryService;
+        private ArticleService articleService;
+
+
+        public CategoryController(IServiceProvider serviceProvider)
+        {
+            categoryService = serviceProvider.GetRequiredService<CategoryService>();
+            articleService = serviceProvider.GetRequiredService<ArticleService>();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
