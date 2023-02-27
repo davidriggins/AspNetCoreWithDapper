6b108262134d9130a39c51c610a8e4d66269ac88
David Riggins <driggins@esri.com>
2023-02-27 11:12:48 -0800

    Lesson 156 End. Create Right Side Components Class.

---
+++ DapperFantom/DapperFantom/ViewComponents/RightSide.cs
@@ -0,0 +1,34 @@
+﻿using DapperFantom.Entities;
+using DapperFantom.Models;
+using DapperFantom.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperFantom.ViewComponents
+{
+    public class RightSide : ViewComponent
+    {
+        private CategoryService categoryService;
+        private ArticleService articleService;
+
+        public RightSide(IServiceProvider serviceProvider)
+        {
+            categoryService = serviceProvider.GetRequiredService<CategoryService>();
+            articleService = serviceProvider.GetRequiredService<ArticleService>();
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+            List<Category> categories = categoryService.GetAll();
+            List<Article> articles = articleService.GetAll().Take(4).OrderByDescending(x => x.Hit).ToList();
+
+            GeneralViewModel model = new GeneralViewModel
+            {
+                CategoryList = categories,
+                ArticleList = articles
+            };
+
+            return View(model);
+        }
+    }
+}
