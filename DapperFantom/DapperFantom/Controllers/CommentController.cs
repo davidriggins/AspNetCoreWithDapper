9776a2c29330397607c63cd952fb5244fe84ae08
David Riggins <driggins@esri.com>
2023-02-27 10:14:05 -0800

    Lesson 152 End. Create Comment Controller.

---
+++ DapperFantom/DapperFantom/Controllers/CommentController.cs
@@ -0,0 +1,49 @@
+﻿using DapperFantom.Entities;
+using DapperFantom.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DapperFantom.Controllers
+{
+    public class CommentController : Controller
+    {
+        private CommentService commentService;
+        private ArticleService articleService;
+
+        public CommentController(IServiceProvider serviceProvider)
+        {
+            commentService = serviceProvider.GetRequiredService<CommentService>();
+            articleService = serviceProvider.GetRequiredService<ArticleService>();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add(string id, IFormCollection form)
+        {
+            Article article = articleService.GetByGuid(id);
+            Comment comment = new Comment
+            {
+                ArticleId = article.ArticleId,
+                CommentText = form["message"],
+                Email = form["email"],
+                Name = form["name"],
+                CreatedDate = DateTime.Now,
+                ModifiedDate = DateTime.Now,
+                Status = 1
+            };
+
+            int result = commentService.Add(comment);
+            if (result > 0)
+            {
+                return Ok(new { success = "true" });
+            }
+            else
+            {
+                return Ok(new {success = "false"});
+            }
+        }
+    }
+}
